c90746e84c558ba48ec387ebb2f2a454f252219e
Xiaoxi <1135022744@qq.com>
2018-06-27 11:17:03 +0800

    主页完成

---
+++ Lshopping/js/index.js
@@ -0,0 +1,27 @@
+$(function () {
+
+  //渲染导航栏
+  Route.getindexmenu (function (info) {
+
+    // console.log(info);
+    $(".m_nav ul").html(template("navTpl", info));
+
+    $("#show").on("click", function () {
+
+      $("#show").nextAll().toggleClass("hide");
+
+    });
+
+  });
+
+  //渲染产品列表
+  Route.getmoneyctrl (function (pageid) {
+
+    console.log(pageid);
+    $(".m_product ul").html(template("productTpl", pageid));
+
+  });
+
+  
+
+})
