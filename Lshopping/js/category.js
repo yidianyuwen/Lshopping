17bd34503765e30f6a2bdf00b51a4c192c180b4d
Xiaoxi <1135022744@qq.com>
2018-06-28 16:11:37 +0800

    分类页分类列表完成

---
+++ Lshopping/js/category.js
@@ -0,0 +1,33 @@
+$(function () {
+
+  Route.getcategorytitle(function (title) {
+
+    // console.log(title);
+    $(".category_title").html(template("titleTpl", title));
+
+    $(".category_title > li > a").on("click", function () {
+
+      var $that = $(this);
+      // console.log(that);
+      var id = $that.data("title-id");
+      console.log(id);
+
+      $that.parent().find('ul').toggleClass('hide');
+
+      //判断有没有获取到详细分类，若有，则不再请求数据
+      if ($($that).parent().find(".category_content").children().length == 0) {
+
+        Route.getcategory(id, function (content) {
+
+          console.log(content, $that); 
+          $($that).parent().find(".category_content").html(template("contentTpl", content));
+  
+        });
+
+      }
+
+    });
+
+  });
+
+});
